1a987aa7936993e975e8cffdc9e0415eef0fd94f
Nikolay Ivanov <eoneof@yandex.ru>
2022-08-21 22:51:24 +0300

    feat: connect to db

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
