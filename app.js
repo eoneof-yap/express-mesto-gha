00ae8017f63622f910c7292b43a5180f4232760b
Nikolay Ivanov <eoneof@yandex.ru>
2022-08-30 22:29:05 +0300

    fix: move fake auth data back to main
    according to autotests

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const { requestLogger } = require('./utils/loggers');
-const { tempAuth } = require('./utils/constants');
 const userRouter = require('./routers/users');
 const cardsRouter = require('./routers/cards');
 const notFoundRouter = require('./routers/404');
@@ -17,7 +16,13 @@ app.use(express.json()); // body-parser is bundled with Express >4.16
 app.use(express.urlencoded({ extended: true }));
 
 // hardcoded user id
-app.use(tempAuth);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '630d25a903576ab62032ca24',
+  };
+
+  next();
+});
 
 app.use(userRouter); // app.js <= /routes <= /controllers <= /models
 app.use(cardsRouter);
--- utils/constants.js
+++ utils/constants.js
@@ -5,20 +5,10 @@ const BAD_REQUEST = 400;
 const NOT_FOUND = 404;
 const SERVER_ERROR = 500;
 
-// TODO: remove
-const tempAuth = (req, res, next) => {
-  req.user = {
-    _id: '630d25a903576ab62032ca24',
-  };
-
-  next();
-};
-
 module.exports = {
   OK,
   CREATED,
   BAD_REQUEST,
   NOT_FOUND,
   SERVER_ERROR,
-  tempAuth,
 };
