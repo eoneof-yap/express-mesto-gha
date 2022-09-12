d71f2b79d1da7361a94ad8bef904d4ea23b28613
Nikolay Ivanov <eoneof@yandex.ru>
2022-09-12 23:11:34 +0300

    feat: add default values to user schema

--- models/user.js
+++ models/user.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose');
 
+const { DEFAULT_NAME, DEFAULT_ABOUT, DEFAULT_AVATAR } = require('../utils/constants');
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
     required: true,
+    default: DEFAULT_NAME,
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
     required: true,
+    default: DEFAULT_ABOUT,
   },
   avatar: {
     type: String,
     required: true,
+    default: DEFAULT_AVATAR,
   },
   email: {
     type: String,
--- utils/constants.js
+++ utils/constants.js
@@ -4,9 +4,16 @@ const BAD_REQUEST = 400;
 const NOT_FOUND = 404;
 const SERVER_ERROR = 500;
 
+const DEFAULT_NAME = 'Жак-Ив Кусто';
+const DEFAULT_ABOUT = 'Исследователь';
+const DEFAULT_AVATAR = 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png';
+
 module.exports = {
   CREATED,
   BAD_REQUEST,
   NOT_FOUND,
   SERVER_ERROR,
+  DEFAULT_NAME,
+  DEFAULT_ABOUT,
+  DEFAULT_AVATAR,
 };
