4ef8af9e5f3aa6a0fa1b0c170c9f9b83fc5aea76
Nikolay Ivanov <eoneof@yandex.ru>
2022-09-12 22:46:52 +0300

    feat: add user credentials

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,5 @@
 const {
-  CREATED,
-  BAD_REQUEST,
-  NOT_FOUND,
-  SERVER_ERROR,
+  CREATED, BAD_REQUEST, NOT_FOUND, SERVER_ERROR,
 } = require('../utils/constants');
 const User = require('../models/user');
 
@@ -28,9 +25,7 @@ const getUserById = (req, res) => {
     })
     .catch((err) => {
       if (err.kind === 'ObjectId') {
-        res
-          .status(BAD_REQUEST)
-          .send({ message: 'Неверный ID', error: err.message });
+        res.status(BAD_REQUEST).send({ message: 'Неверный ID', error: err.message });
         return;
       }
       res.status(SERVER_ERROR).send({
@@ -40,14 +35,14 @@ const getUserById = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(CREATED).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res
-          .status(BAD_REQUEST)
-          .send({ message: 'Ошибка в запросе', error: err.message });
+        res.status(BAD_REQUEST).send({ message: 'Ошибка в запросе', error: err.message });
         return;
       }
       res.status(SERVER_ERROR).send({
@@ -76,15 +71,11 @@ const updateUser = (req, res) => {
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res
-          .status(BAD_REQUEST)
-          .send({ message: 'Ошибка в запросе', error: err.message });
+        res.status(BAD_REQUEST).send({ message: 'Ошибка в запросе', error: err.message });
         return;
       }
       if (err.kind === 'ObjectId') {
-        res
-          .status(BAD_REQUEST)
-          .send({ message: 'Неверный ID', error: err.message });
+        res.status(BAD_REQUEST).send({ message: 'Неверный ID', error: err.message });
         return;
       }
       res.status(SERVER_ERROR).send({
@@ -113,9 +104,7 @@ const updateUserAvatar = (req, res) => {
     })
     .catch((err) => {
       if (err.kind === 'ObjectId') {
-        res
-          .status(BAD_REQUEST)
-          .send({ message: 'Неверный ID', error: err.message });
+        res.status(BAD_REQUEST).send({ message: 'Неверный ID', error: err.message });
         return;
       }
       res.status(SERVER_ERROR).send({
