7e919cc75fa283e7f55ba602bbb63e4d38aa6745
Nikolay Ivanov <eoneof@yandex.ru>
2022-09-14 17:38:33 +0300

    refactor: handle auth middlewear errors globally

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
+const { TOKEN_PREFIX, AUTH_REQUIRED_TEXT } = require('../utils/constants');
+
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
 const {
   // TODO: remove secter value
   JWT_SECRET = '41f2274f52d9ad3f094d4378b763b7ad2e870e4a1a283c59c1d91a0a0336b026',
 } = process.env;
-const { UNAUTHORIZED, TOKEN_PREFIX, AUTH_REQUIRED_TEXT } = require('../utils/constants');
 
-const handleAuthError = (res) => res.status(UNAUTHORIZED).send({ message: AUTH_REQUIRED_TEXT });
 const extractBearerToken = (header) => header.replace('Bearer ', '');
 
 // eslint-disable-next-line consistent-return
@@ -15,7 +17,7 @@ const authorize = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith(TOKEN_PREFIX)) {
-    return handleAuthError(res);
+    next(new UnauthorizedError(AUTH_REQUIRED_TEXT));
   }
 
   const token = extractBearerToken(authorization);
@@ -24,7 +26,7 @@ const authorize = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return handleAuthError(res);
+    next(new UnauthorizedError(AUTH_REQUIRED_TEXT));
   }
   req.user = payload;
 
