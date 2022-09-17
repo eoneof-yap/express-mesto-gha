d9b65210b63aa59b1d0906a46ec6c45c4874b868
Nikolay Ivanov <eoneof@yandex.ru>
2022-09-17 23:14:14 +0300

    feat: make loggers more useful

--- utils/loggers.js
+++ utils/loggers.js
@@ -1,6 +1,22 @@
+const getTimeStamp = () => new Date().toUTCString();
+
+// Console colors
+
+const red = '\x1b[31m';
+const blue = '\x1b[34m';
+const yellow = '\x1b[33m';
+
+const errorLogger = (message) => {
+  console.log(red, `${getTimeStamp()} ${message}`);
+};
+
+const eventLogger = (message) => {
+  console.log(yellow, `${getTimeStamp()} ${message}`);
+};
+
 const requestLogger = (req, res, next) => {
-  console.log(req.method, req.url);
+  console.log(blue, `${getTimeStamp()} ${req.method} ${req.url}`);
   next();
 };
 
-module.exports = { requestLogger };
+module.exports = { requestLogger, eventLogger, errorLogger };
