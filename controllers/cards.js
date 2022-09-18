a9c9ef65cd0e0e0c7599d7f07255c662ca89a81f
Nikolay Ivanov <eoneof@yandex.ru>
2022-09-18 15:56:45 +0300

    refactor: remove if else statement

--- controllers/cards.js
+++ controllers/cards.js
@@ -28,9 +28,9 @@ const createCard = (req, res, next) => {
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError(BAD_REQUEST_TEXT));
-      } else {
-        next(err);
+        return;
       }
+      next(err);
     });
 };
 
